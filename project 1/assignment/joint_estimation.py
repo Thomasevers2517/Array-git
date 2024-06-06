d2d9768322b2f8aec6dafc6e6ad1711132197f5c
Thomasevers2517 <thomasevers9@gmail.com>
2024-06-06 15:14:17 +0200

    sortoff working joint estimation, but results not diagonal yet. I need to fix cus somethign wrong

--- project 1/assignment/joint_estimation.py
+++ project 1/assignment/joint_estimation.py
@@ -4,6 +4,6 @@ from src.joint_diagonalisation import joint_diag
 import numpy as np
 
 X = generate_data_khatri_rao(3, 500, [30, 50], [0.005, 0.002], [100, 100])
-
+print(X.shape)
 V, D = joint_diag(np.array(X), 1e-10)
 print(V)
